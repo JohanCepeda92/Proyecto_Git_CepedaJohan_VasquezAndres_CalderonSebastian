3265f047a8c99c3715bb66e5f71a22a2e2263e56
JohanCepeda92 <Johannicolascepeda92@gmail.com>
2026-09-20 18:04:44 -0500

    feat: Agregué registro de items

--- inventario.py
+++ inventario.py
@@ -15,3 +15,28 @@ def cargar_inventario():
 def guardar_inventario(inventario):
     with open(ARCHIVO_INVENTARIO, "w", encoding="utf-8") as archivo:
         json.dump(inventario, archivo, indent=4, ensure_ascii=False)
+
+def registrar_item():
+    inventario = cargar_inventario()
+
+    codigo = input("Ingrese el código del ítem: ").strip()
+    titulo = input("Ingrese el título: ").strip()
+    autor = input("Ingrese el autor: ").strip()
+    categoria = input("Ingrese la categoría: ").strip()
+    cantidad = int(input("Ingrese la cantidad total: "))
+    ubicacion = input("Ingrese la ubicación: ").strip()
+
+    nuevo_item = {
+        "codigo": codigo,
+        "titulo": titulo,
+        "autor": autor,
+        "categoria": categoria,
+        "cantidad_total": cantidad,
+        "cantidad_disponible": cantidad,
+        "ubicacion": ubicacion
+    }
+
+    inventario.append(nuevo_item)
+    guardar_inventario(inventario)
+
+    print(f'Ítem "{titulo}" registrado exitosamente. Disponibles: {cantidad}')
