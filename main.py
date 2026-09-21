63a469bc9acbf044c57ce0f07e903cc040aae964
c4lde <sebastian.c.silva@hotmail.com>
2026-09-21 18:40:35 -0500

    feat: Conecté préstamos y devoluciones al menú

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-print("BiblioStock - Biblioteca Horizonte")
-print("Sistema de inventario y préstamos")
-
-def mostrar_menu():
-    print("\n==========================================")
-    print("BIBLIOSTOCK CLI - BIBLIOTECA HORIZONTE")
-    print("==========================================")
-    print("1. Registrar ítem")
-    print("2. Listar ítems")
-    print("3. Buscar ítem")
-    print("4. Registrar préstamo")
-    print("5. Registrar devolución")
-    print("6. Salir")
-    print("==========================================")
-
 from inventario import registrar_item, listar_items, buscar_item
+from prestamos import registrar_prestamo, registrar_devolucion
 
 
 print("BiblioStock - Biblioteca Horizonte")
@@ -49,10 +35,10 @@ def ejecutar_menu():
             buscar_item()
 
         elif opcion == "4":
-            print("Función de préstamos próximamente.")
+            registrar_prestamo()
 
         elif opcion == "5":
-            print("Función de devoluciones próximamente.")
+            registrar_devolucion()
 
         elif opcion == "6":
             print("Gracias por utilizar BiblioStock.")
